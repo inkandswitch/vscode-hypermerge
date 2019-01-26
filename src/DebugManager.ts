2cb450db3e9cc1b7f48d7089129816393409e6a7
Jeff Peterson <jeff@yak.sh>
2019-01-26 12:02:28 -0800

    Fix debug logging

--- src/DebugManager.ts
+++ src/DebugManager.ts
@@ -1,5 +1,5 @@
 import { Disposable, OutputChannel, workspace } from "vscode"
-import Debug from "debug"
+import Debug from "hypermerge/node_modules/debug"
 import { format } from "util"
 import DisposableCollection from "./DisposableCollection"
 
